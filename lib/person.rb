2143c41845f1f4bbd31a5d4946ace2c80ff125f0
Ali Karimi <ajkarimi7@gmail.com>
2019-09-11 22:24:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,19 @@
 class Person
-  
-  def initialize(name, job)
-    
-    @name = name
-    @job = job
-    
+
+  def name
+    @name
   end 
   
+  def name=(name)
+    @name = name
+  end 
   
+  def job
+    @job
+  end 
+  
+  def job=(job)
+    @job = job
+  end 
   
 end 
